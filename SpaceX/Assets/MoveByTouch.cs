7ff7b988011008bfdb57412c5693351de23f49d3
Dilmurod777 <valiev.dilmurod1998@gmail.com>
2020-07-28 14:42:45 +0500

    add touch screen control

--- SpaceX/Assets/MoveByTouch.cs
+++ SpaceX/Assets/MoveByTouch.cs
@@ -5,25 +5,49 @@ using UnityEngine;
 
 public class MoveByTouch : MonoBehaviour
 {
-    private float speed = 10f;
-    private Vector2 pointA;
-    private Vector2 pointB;
+    private Vector3 velocity = Vector3.zero;
+    private float speed = 15f;
+    private float acceleration = 25f;
+    private float rotationSpeed = 15f;
 
-    void Update()
+
+    private void Update()
+    {
+        //transform.Translate(velocity * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
     {
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
 
-            if (touch.phase == TouchPhase.Began)
+            if (touch.phase == TouchPhase.Moved)
             {
-                pointA = transform.position;
+                // move to the touch point
+                float touchPointX = Camera.main.ScreenToWorldPoint(touch.position).x;
+                float touchPointY = Camera.main.ScreenToWorldPoint(touch.position).y;
+                float deltaX = touchPointX - transform.position.x;
+                float deltaY = touchPointY - transform.position.y;
+                //velocity.x = Mathf.Clamp(Mathf.MoveTowards(velocity.x, speed * deltaX, acceleration * Time.fixedDeltaTime), -speed, speed);
+                //velocity.y = Mathf.Clamp(Mathf.MoveTowards(velocity.y, speed * deltaY, acceleration * Time.fixedDeltaTime), -speed, speed);
+                transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(deltaX, deltaY) * speed);
+
+
+                // rotate to the touch point
+                float angle = Vector3.SignedAngle(transform.up, new Vector3(deltaX, deltaY, transform.position.z).normalized, transform.forward);
+                transform.Rotate(Vector3.forward, angle * rotationSpeed * Time.fixedDeltaTime);
             }
-            else if (touch.phase == TouchPhase.Moved)
+            else if (touch.phase == TouchPhase.Ended)
             {
-                Vector3 direction = Vector3.ClampMagnitude(Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.transform.position.z)) - transform.position, 1.0f);
-                transform.Translate(direction * speed * Time.deltaTime);
+                velocity.x = Mathf.MoveTowards(velocity.x, 0, acceleration * Time.deltaTime);
+                velocity.y = Mathf.MoveTowards(velocity.y, 0, acceleration * Time.deltaTime);
             }
+        }
+        else
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, acceleration * Time.deltaTime);
+            velocity.y = Mathf.MoveTowards(velocity.y, 0, acceleration * Time.deltaTime);
         }
     }
 }
