4251ace279df87f1dc736e3da9ca74cddb149085
Dilmurod777 <valiev.dilmurod1998@gmail.com>
2020-07-28 22:03:08 +0500

    update code

---
+++ SpaceX/Assets/ObjectDetector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float requiredHeight = 20;
+        float requiredWidth = requiredHeight * Camera.main.aspect;
+
+        float height = Camera.main.fieldOfView;
+
+        gameObject.GetComponent<BoxCollider>().size = new Vector3(requiredWidth, requiredHeight, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
