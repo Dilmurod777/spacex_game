0ca9f31b02e069cbf335acf7002f15b111ae8e9e
Dilmurod777 <valiev.dilmurod1998@gmail.com>
2020-07-28 20:31:29 +0500

    modify touch contorl, camera

--- SpaceX/Assets/MoveByTouch.cs
+++ SpaceX/Assets/MoveByTouch.cs
@@ -2,8 +2,10 @@
 
 public class MoveByTouch : MonoBehaviour
 {
-    private float speed = 100f;
-    private float rotationSpeed = 15f;
+    private float speed = 125f;
+    private float rotationSpeed = 10f;
+    float touchPointX;
+    float touchPointY;
 
     private void FixedUpdate()
     {
@@ -13,17 +15,23 @@ public class MoveByTouch : MonoBehaviour
 
             if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
             {
+                Vector3 touchPosInWorldSpace = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.farClipPlane));
                 // move to the touch point
-                float touchPointX = Camera.main.ScreenToWorldPoint(touch.position).x;
-                float touchPointY = Camera.main.ScreenToWorldPoint(touch.position).y;
-                float deltaX = touchPointX - transform.position.x;
-                float deltaY = touchPointY - transform.position.y;
+                touchPointX = touchPosInWorldSpace.x;
+                touchPointY = touchPosInWorldSpace.y;
+
+                float cameraHeight = 2f * Camera.main.orthographicSize;
+                float cameraWidth = cameraHeight * Camera.main.aspect;
+                float requiredHeight = 20;
+                float requiredWidth = requiredHeight * Camera.main.aspect;
+                float deltaX = touchPointX * requiredWidth / cameraWidth;
+                float deltaY = touchPointY * requiredHeight / cameraHeight;
 
                 transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(deltaX, deltaY).normalized * speed);
 
                 // rotate to the touch point
                 float angle = Vector3.SignedAngle(transform.up, new Vector3(deltaX, deltaY, transform.position.z).normalized, transform.forward);
-                transform.Rotate(Vector3.forward, angle * rotationSpeed * Time.fixedDeltaTime);
+                transform.Rotate(0, 0, angle * rotationSpeed * Time.fixedDeltaTime);
             }
         }
     }
