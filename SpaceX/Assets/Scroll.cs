63b628d661f038f29f085bf1279e9de8c17a81a9
Dilmurod777 <valiev.dilmurod1998@gmail.com>
2020-08-24 00:13:00 +0500

    add scroll

---
+++ SpaceX/Assets/Scroll.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    public static bool created;
+    
+    private void Awake()
+    {
+        if (!created)
+        {
+            created = true;
+            
+        }
+        else
+        {
+            GetComponent<Animator>().enabled = false;
+        }
+    }
+}
