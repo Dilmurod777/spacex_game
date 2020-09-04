61ed5dfb46619fb51b5235405a92fb946b01c447
Dilmurod777 <valiev.dilmurod1998@gmail.com>
2020-09-04 22:05:55 +0500

    test

--- SpaceX/Assets/FireCometFalling.cs
+++ SpaceX/Assets/FireCometFalling.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.PlayerLoop;
+﻿using UnityEngine;
 
 public class FireCometFalling : MonoBehaviour
 {
     private Animator _animator;
     private GameObject _player;
+    private bool _triggered = false;
 
     private void Start()
     {
@@ -17,18 +14,20 @@ public class FireCometFalling : MonoBehaviour
 
     private void FixedUpdate()
     {
-     
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        _animator.SetTrigger("startFalling");
-        Debug.Log("COMET TRIGGER ENTER " + name);
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        _animator.SetTrigger("resetFalling");
-        Debug.Log("COMET TRIGGER EXIT | " + name);
+        var distance = (_player.transform.position - transform.position).magnitude;
+        Debug.Log(distance);
+        if (distance < 100)
+        {
+            if (!_triggered)
+            {
+                _animator.SetTrigger("startFalling");
+                _triggered = true;
+            }
+        }
+        else
+        {
+            _triggered = false;
+            _animator.SetTrigger("resetFalling");
+        }
     }
 }
